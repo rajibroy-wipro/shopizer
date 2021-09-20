8a77830379a5d512d1fb4d09420cd3ced3747c2a
rajibroy-wipro <54103367+rajibroy-wipro@users.noreply.github.com>
2021-09-20 11:44:32 +0530

    Update ContactController.java

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
@@ -65,6 +65,8 @@ public class ContactController extends AbstractController {
 	@RequestMapping("/shop/store/contactus.html")
 	public String display(Model model, HttpServletRequest request, HttpServletResponse response, Locale locale) throws Exception {
 		
+		System.out.println("changes");
+		
 		MerchantStore store = (MerchantStore)request.getAttribute(Constants.MERCHANT_STORE);
 		
 		model.addAttribute("googleMapsKey",googleMapsKey);
