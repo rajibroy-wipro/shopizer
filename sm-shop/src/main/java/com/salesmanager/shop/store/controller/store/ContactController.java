f582223b8b8caa590f06472a2dda3e1af51b05b3
rajibroy-wipro <54103367+rajibroy-wipro@users.noreply.github.com>
2022-03-10 02:11:24 +0530

    Update ContactController.java

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
@@ -80,6 +80,8 @@ public class ContactController extends AbstractController {
 		
 		System.out.println("TIA - changes for parallel test");
 		
+		System.out.println("changes for TIA");
+		
 		MerchantStore store = (MerchantStore)request.getAttribute(Constants.MERCHANT_STORE);
 		
 		model.addAttribute("googleMapsKey",googleMapsKey);
